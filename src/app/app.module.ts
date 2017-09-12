f66fbb0821e04a51fec9326dec1373d775a9ecc4
jaisil007 <jaisilchacko@yahoo.com>
2017-09-11 22:18:57 -0500

    template and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonsComponent } from './commons/commons.component';
+import { HeaderComponent } from './commons/header/header.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { WeatherComponent } from './content/weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonsComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
